516de81dfcc94800924298ceee5e5e798c8f425c
Reziero Simeone <rezierosimeone@macbookpro.home>
2025-10-31 12:50:12 -0400

    fix: add missing about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>About Cashup</h1>
+      <p>This is the About page of the Cashup Card Demo MVP.</p>
+    </div>
+  );
+}
